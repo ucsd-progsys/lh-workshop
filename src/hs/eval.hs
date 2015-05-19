357181c768e78a04a4d421ce55583cefaf43d5e2
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-05-19 16:28:32 -0700

    onto eval

--- src/hs/eval.hs
+++ src/hs/eval.hs
@@ -1,23 +1,59 @@
 {-@ LIQUID "--no-termination" @-}
 {-@ LIQUID "--short-names"    @-}
 
-module Eval  where
+--------------------------------------------------------
+-- | Boilerplate
+--------------------------------------------------------
+
+module Eval (Map, Expr (..), eval, evalTop, evalAny) where
 
 import Prelude hiding (lookup)
 import qualified Data.Set as S
 
 {-@ impossible :: {v: _ | false} -> a @-}
+impossible :: String -> a
 impossible = error
 
+type Env = Map Var Expr
+
 plus :: Expr -> Expr -> Expr
+evalTop :: Expr -> Expr
+evalAny :: Map Var Expr -> Expr -> Maybe Expr
+
+
+--------------------------------------------------------
+-- | An Associative Map
+--------------------------------------------------------
 
 data Map k v = Emp
              | Bind k v (Map k v)
                deriving (Eq, Ord, Show)
 
+--------------------------------------------------------
+-- | Set of defined keys
+--------------------------------------------------------
+
+{-@ measure keys @-}
+keys :: (Ord k) => Map k v -> S.Set k
+keys Emp          = S.empty
+keys (Bind k _ m) = add k m
+
+--------------------------------------------------------
+-- | Empty map without any keys
+--------------------------------------------------------
+
 {-@ empty :: {m:_ | noKeys m} @-}
 empty :: Map k v
 empty = Emp
+
+
+{-@ inline noKeys @-}
+noKeys :: (Ord k) => Map k v -> Bool
+noKeys m = keys m == S.empty
+
+--------------------------------------------------------
+-- | Membership test (SKIP?)
+--------------------------------------------------------
 
 member :: (Eq k) => k -> Map k v -> Bool
 member k' (Bind k _ m)
@@ -25,33 +61,32 @@ member k' (Bind k _ m)
   | otherwise = member k' m
 member _  Emp = False
 
-{-@ lookup    :: (Eq k) => k:k -> {m: _ | hasKey k m} -> v @-}
+--------------------------------------------------------
+-- | Lookup binding
+--------------------------------------------------------
+
+{-@ lookup :: (Eq k) => k:_ -> {m: _ | has k m} -> v @-}
 lookup    :: (Eq k) => k -> Map k v -> v
 lookup k' (Bind k v m)
   | k' == k   = v
   | otherwise = lookup k' m
 lookup _  Emp = impossible "lookup"
 
-{-@ insert :: k:_ -> _ -> m:_ -> {v: _ | keys v = addKey k m } @-}
+{-@ inline has @-}
+has :: (Ord k) => k -> Map k v -> Bool
+has k m = S.member k (keys m)
+
+--------------------------------------------------------
+-- | Insert binding
+--------------------------------------------------------
+
+{-@ insert :: k:_ -> _ -> m:_ -> {v: _ | keys v = add k m } @-}
 insert :: k -> v -> Map k v -> Map k v
 insert = Bind
 
-{-@ measure keys @-}
-keys :: (Ord k) => Map k v -> S.Set k
-keys Emp          = S.empty
-keys (Bind k _ m) = addKey k m
-
-{-@ inline addKey @-}
-addKey :: (Ord k) => k -> Map k v -> S.Set k
-addKey k kvs = S.singleton k `S.union` keys kvs
-
-{-@ inline hasKey @-}
-hasKey :: (Ord k) => k -> Map k v -> Bool
-hasKey k m = S.member k (keys m)
-
-{-@ inline noKeys @-}
-noKeys :: (Ord k) => Map k v -> Bool
-noKeys m = keys m == S.empty
+{-@ inline add @-}
+add :: (Ord k) => k -> Map k v -> S.Set k
+add k kvs = S.singleton k `S.union` keys kvs
 
 --------------------------------------------------------
 -- | Values
@@ -59,10 +94,10 @@ noKeys m = keys m == S.empty
 
 type Var  = String
 
-data Expr = Const Int
-          | Var   Var
-          | Plus  Expr Expr
-          | Let   Var  Expr Expr
+data Expr = Val  Int
+          | Var  Var
+          | Plus Expr Expr
+          | Let  Var  Expr Expr
 
 --------------------------------------------------------
 -- | Values
@@ -70,8 +105,8 @@ data Expr = Const Int
 
 {-@ measure isVal @-}
 isVal :: Expr -> Bool
-isVal (Const _) = True
-isVal _         = False
+isVal (Val _) = True
+isVal _       = False
 
 {-@ type Val = {v:Expr | isVal v} @-}
 
@@ -79,9 +114,9 @@ isVal _         = False
 -- | Operating on Values
 --------------------------------------------------------
 
-{-@ plus                 :: Val -> Val -> Val @-}
-plus (Const i) (Const j) = Const (i+j)
-plus _         _         = impossible "plus"
+{-@ plus :: Val -> Val -> Val @-}
+plus (Val i) (Val j) = Val (i+j)
+plus _         _     = impossible "plus"
 
 --------------------------------------------------------
 -- | Environments / Stores
@@ -89,23 +124,23 @@ plus _         _         = impossible "plus"
 
 {-@ type Env = Map Var Val @-}
 
-{-@ eval :: g:Env -> ScopedExpr g -> Val @-}
-eval _ i@(Const _)   = i
+{-@ eval :: Env -> Expr -> Val @-}
+eval _ i@(Val _)     = i
 eval g (Var x)       = lookup x g
-eval g (Plus e1 e2)  = plus  (eval g e1) (eval g e2)
-eval g (Let x e1 e2) = eval g' e2
+eval g (Plus e1 e2)  = plus (eval g e1) (eval g e2)
+eval g (Let x e1 e2) = eval gx e2
   where
-    g'               = insert x v1 g
+    gx               = insert x v1 g
     v1               = eval g e1
 
-{-@ type ScopedExpr G = {e: Expr | wellScoped G e} @-}
 
-{-@ inline wellScoped @-}
-wellScoped g e = free e `S.isSubsetOf` keys g
+--------------------------------------------------------
+-- | Free variables
+--------------------------------------------------------
 
 {-@ measure free @-}
 free               :: Expr -> S.Set Var
-free (Const _)     = S.empty
+free (Val _)       = S.empty
 free (Var x)       = S.singleton x
 free (Plus e1 e2)  = xs1 `S.union`  xs2
   where
@@ -117,5 +152,34 @@ free (Let x e1 e2) = xs1 `S.union` (xs2 `S.difference` xs)
     xs2            = free e2
     xs             = S.singleton x
 
+--------------------------------------------------------
+-- | Well-scoped Expressions
+--------------------------------------------------------
+
+{-@ type ScopedExpr G = {e: Expr | wellScoped G e} @-}
+
+{-@ inline wellScoped @-}
+wellScoped :: Env -> Expr -> Bool
+wellScoped g e = free e `S.isSubsetOf` keys g
 
 
+--------------------------------------------------------
+-- | Top-level Evaluation
+--------------------------------------------------------
+
+{-@ type ClosedExpr = {e: Expr | free e == S.empty} @-}
+
+{-@ evalTop :: ClosedExpr -> Val @-}
+evalTop = eval Emp
+
+
+--------------------------------------------------------
+-- | Checked Top-level Evaluation
+--------------------------------------------------------
+
+{-@ evalAny :: Env -> Expr -> Maybe Val @-}
+evalAny g e
+  | ok        = Just $ eval g e
+  | otherwise = Nothing
+  where
+    ok        = wellScoped g e
